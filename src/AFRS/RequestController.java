41c97f7a29f71bf5c9e892f897c81d8cf24ec285
Wm. Blake Sullivan <wbsllvn@csh.rit.edu>
2017-10-16 01:54:47 -0400

    added AFRS main package, removed duplicate directories after refactor

---
+++ src/AFRS/RequestController.java
@@ -0,0 +1,43 @@
+package AFRS;
+
+import AFRS.Requests.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class RequestController {
+
+    private static HashMap<String, Request> requests;
+    private static ReservationDatabase reservationDB;
+
+    public RequestController() {
+        reservationDB = new ReservationDatabase();
+        reservationDB.startUp();
+        requests = new HashMap<>();
+        requests.put("airport", new AirportRequest());
+        requests.put("delete", new DeleteRequest(reservationDB));
+        requests.put("info", new InfoRequest(reservationDB));
+        requests.put("reserve", new ReserveRequest(reservationDB));
+        requests.put("retrieve", new RetrieveRequest(reservationDB));
+    }
+
+    public ArrayList<String> parse(String str) {
+        String input = str.substring(0, str.length() - 1);
+        String[] requestAndParams = input.split(",");
+        String request = requestAndParams[0];
+        if(request.equals("quit")) {
+            reservationDB.shutDown();
+
+            ArrayList<String> response = new ArrayList<>();
+            response.add("quit");
+            return response;
+        }
+        String[] params = new String[requestAndParams.length - 1];
+        for(int i = 1; i < requestAndParams.length; i++) {
+            params[i-1] = requestAndParams[i];
+        }
+
+        return requests.get(request).doRequest(params);
+    }
+
+}
--- src/Strategy/ItinerarySort.java
+++
@@ -1,9 +0,0 @@
-package Strategy;
-
-import AFRS.Model.Itinerary;
-
-import java.util.ArrayList;
-
-public interface ItinerarySort {
-    public ArrayList<Itinerary> doSort(ArrayList<Itinerary> itineraryList);
-}
