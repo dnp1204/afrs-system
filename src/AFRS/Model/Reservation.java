f57ee142280409565f9100cb9dbb3f4f4bba8591
Jack Corrigan <jcorrigan14@gmail.com>
2017-10-16 15:42:56 -0400

    Completed Delete() and Reserve(). began StartUp() and retrieve()

--- src/AFRS/Model/Reservation.java
+++ src/AFRS/Model/Reservation.java
@@ -1,6 +1,6 @@
 package AFRS.Model;
 
-public class Reservation {
+public class Reservation implements Comparable<Reservation> {
     private String passengerName;
     private Itinerary itinerary;
 
@@ -19,4 +19,19 @@ public class Reservation {
     public void setItinerary(Itinerary itinerary) {
         this.itinerary = itinerary;
     }
+
+    //this allows reservations to be sorted by origin flight. Necessary for retrieve command
+    @Override
+    public int compareTo(Reservation reservation) {
+        String compare_origin = reservation.getItinerary().getOrigin();
+        String my_origin = this.getItinerary().getOrigin();
+
+
+        /* For Ascending order*/
+        return compare_origin.compareToIgnoreCase(my_origin);
+
+        /* For Descending order do like this */
+            //return compareage-this.studentage;
+        }
+
 }
--- src/AFRS/ReservationDatabase.java
+++ src/AFRS/ReservationDatabase.java
@@ -1,12 +1,22 @@
 package AFRS;
 
-import Model.Itinerary;
-import Model.Reservation;
+import AFRS.Model.Itinerary;
+import AFRS.Model.Reservation;
+import com.sun.org.apache.regexp.internal.RE;
+import jdk.nashorn.internal.runtime.arrays.ArrayIndex;
+import org.omg.CORBA.OBJECT_NOT_EXIST;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class ReservationDatabase {
     private ArrayList<Reservation> reservationList = new ArrayList<Reservation>();
     //this needs to read from file to populate and be persistent reservation database
-    public ArrayList<Itinerary> recentInventoryList = new ArrayList<Itinerary>();
+    public ArrayList<Itinerary> recentItineraryList = new ArrayList<Itinerary>();
 
 
 //- startUp() : void
@@ -21,24 +31,27 @@ public class ReservationDatabase {
     private void startUp(){
         try {
             //ArrayList<Reservation> reservationList = new ArrayList<Reservation>();
-            BufferedReader  inFile = new BufferedReader (new FileReader ("ReservationDB.txt"));
+            BufferedReader inFile = new BufferedReader(new FileReader("ReservationDB.txt"));
             String inputLine;
 
-            while ((inputLine = inFile.readLine())!=null) {
+            while ((inputLine = inFile.readLine()) != null) {
                 Reservation new_reservation = new Reservation();
                 String[] reservationInfo = inputLine.split(",");
                 //split into each reservation
                 //then split passenger name and Itinerary string
                 new_reservation.setPassengerName(reservationInfo[0])
                 //call Itinerary constructor with string parameter
-                Itinerary new_itinerary_from_string = new Itinerary(reservationInfo[1]);
+                Itinerary new_itinerary_from_string = new Itinerary(reservationInfo[1]); //TODO: this
                 new_reservation.setItinerary(new_itinerary_from_string);
 
                 reservationList.add(new_reservation);
+                System.out.print(new_reservation);
             }
-            System.out.print(student);
+
         } catch (FileNotFoundException e) {
             // this is what happens if there is no previous reservation list to populate db
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -50,9 +63,96 @@ public class ReservationDatabase {
     //called by client to reserve an itinerary that was just queried
     //params: itinerary (Itinerary) - the itinerary to be reserved4
 
-    private void reserve(Itinerary itinerary, String pasengerName) {
+    private String reserve(int id, String passengerName) {
+
+        // check if id is valid
+        // then check if this reservation already exists
+        try{
+            Itinerary itinerary = recentItineraryList.get(id + 1); // use id + 1 to handle 0-based array index
+            for (Reservation r : reservationList) {
+                if (r.getPassengerName().equals(passengerName)){
+                    Itinerary check_itinerary = r.getItinerary();
+                    if (check_itinerary.toString().equals(itinerary.toString())){
+                        return "error,duplicate reservation";
+
+                    }
+                }
+
+            }
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+            return "error, invalid id";
+        }
+
+        Reservation new_reservation = new Reservation();
+        new_reservation.setPassengerName(passengerName);
+        new_reservation.setItinerary(recentItineraryList.get(id + 1));
+        //use id + 1 to handle the 0-based index of array
+        reservationList.add(new_reservation);
+        return("reserve,successful");
+
+        //TODO; error handle duplicate reservation
+    }
+
+    public String delete(String passenger, String origin, String destination) {
+        for (Reservation r : reservationList){
+            if (r.getPassengerName().equals(passenger)){
+                Itinerary itinerary = r.getItinerary();
+                if (itinerary.getOrigin().equals(origin) && itinerary.getDestination().equals(destination)){
+                    reservationList.remove(r);
+                    return "delete,successful";
+                }
+            }
+        }
+        return "error,reservation not found";
 
     }
+
+    public ArrayList<Reservation> retrieve(String passenger){
+        ArrayList<Reservation> temp_list = new ArrayList<Reservation>();
+        for (Reservation r : reservationList){
+            if (r.getPassengerName().equals(passenger)){
+                temp_list.add(r);
+            }
+        }
+        Collections.sort(temp_list);
+        return temp_list;
+    }
+    //how to tell if i am given just origin or just destination. It would just be a string
+    public ArrayList<Reservation> retrieve(String passenger, String origin){
+        ArrayList<Reservation> temp_list = new ArrayList<Reservation>();
+        for (Reservation r : reservationList){
+            if (r.getPassengerName().equals(passenger)){
+                if (r.getItinerary().getOrigin().equals(origin)) {
+                    temp_list.add(r);
+                }
+            }
+        }
+        Collections.sort(temp_list);
+        return temp_list;
+
+    }
+
+    public ArrayList<Reservation> retrieve(String passenger, String destination){
+       //same as above
+        return;
+    }
+
+    public ArrayList<Reservation> retrieve(String passenger, String origin, String destination){
+        ArrayList<Reservation> temp_list = new ArrayList<Reservation>();
+        for (Reservation r : reservationList){
+            if (r.getPassengerName().equals(passenger)){
+                if (r.getItinerary().getOrigin().equals(origin) && r.getItinerary().getDestination().equals(destination)) {
+                    temp_list.add(r);
+                }
+            }
+        }
+        Collections.sort(temp_list);
+        return temp_list;
+
+    }
+
 
 }
 
